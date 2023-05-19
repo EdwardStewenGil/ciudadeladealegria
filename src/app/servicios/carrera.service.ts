c6a7afac782fe437ef22f7cee73805e48168cdf2
EDUAR STIVEN GIL BUITRAGO <edward>
2023-05-18 22:29:58 -0500

    cambios finales asd

--- src/app/servicios/carrera.service.ts
+++ src/app/servicios/carrera.service.ts
@@ -14,8 +14,8 @@ export class CarreraService {
   respuesta = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
-  //hosteo = 'http://localhost:3306/'
-  hosteo = 'https://ccdepor.herokuapp.com/'
+  hosteo = 'http://localhost:3306/'
+ // hosteo = 'https://ccdepor.herokuapp.com/'
 
 
   constructor(private httpClient: HttpClient) { }
@@ -25,6 +25,15 @@ export class CarreraService {
 
     return this.httpClient.post<any>(
       this.hosteo + 'caliciudaddeportiva/createUserCarrera ',
+      data,
+      this.respuesta
+    );
+  }
+
+  public createUserCarrera7k(data: UsuarioDto): Observable<boolean> {
+
+    return this.httpClient.post<any>(
+      this.hosteo + 'caliciudaddeportiva/createUserCarrera7k ',
       data,
       this.respuesta
     );
